e5db6cedac2bb579b59ddb58a0b2adf6cc1b3e65
Adam Taylor <Adam.Taylor@nottingham.ac.uk>
2020-06-11 10:52:17 +0100

    Refactor tabs into functions

--- app/dash_app.py
+++ app/dash_app.py
@@ -8,7 +8,6 @@ from dash.dependencies import Input, Output
 
 import pandas as pd
 
-
 app_title = 'Pimms Dashboard'
 
 # Define local paths
@@ -61,6 +60,94 @@ def create_header(title):
         )
 
 
+# First tab on control panel. About App and link to paper.
+def control_about_tab():
+    return html.Div(children=[
+                html.H3(children="PIMMS",
+                        style={'font-weight': '400', 'font-size': '20pt',
+                               'margin-bottom': '30px', 'text-align': 'center'}
+                        ),
+                dcc.Markdown("""
+                ### Pragmatic Insertional Mutation Mapping system mapping pipeline
+                The PIMMS (Pragmatic Insertional Mutation Mapping System) pipeline has been
+                developed for simple conditionally essential genome discovery experiments in bacteria.
+                Capable of using raw sequence data files alongside a FASTA sequence of the
+                reference genome and GFF file, PIMMS will generate a tabulated output of each coding
+                sequence with corresponding mapped insertions accompanied with normalized results
+                enabling streamlined analysis. This allows for a quick assay of the genome to identify
+                conditionally essential genes on a standard desktop computer prioritizing results for
+                further investigation.
+                """,
+                style={'font-weight': '200', 'font-size': '10pt', 'line-height':'1.6'}),
+                html.Div([
+                    'Reference: ',
+                    html.A('PIMMS paper',
+                           href='https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4391243/pdf/fgene-06-00139.pdf',
+                           style={'color': '#FFFFFF'})
+                ]),
+            ], style={'margin-left': '10px', 'text-align': 'Left'}),
+
+
+# Second tab on control panel select data for processing.
+def control_data_tab():
+    return html.Div(children=[
+                html.Div(children=[
+                    html.Div(className='bac-dropdown-name', children='Bacterial Species',
+                             style={'margin-right': '2em', 'verticalAlign': "middle"}),
+                    dcc.Dropdown(
+                        id='bac-species-dropdown',
+                        options=[
+                            {'label': 'Species A', 'value': 'specA'},
+                            {'label': 'Species B', 'value': 'specB'},
+                            {'label': 'Species C', 'value': 'specC'},
+                            {'label': 'Species D', 'value': 'specD'}
+                        ],
+                        value='specA',
+                        style={'width': '140px', 'verticalAlign': "middle",
+                               'border': 'solid 1px #545454', 'color': 'black'}
+                    )
+                ], style={'display': 'flex', 'marginTop': '5px', 'justify-content': 'center',
+                          'align-items': 'center'}),
+                html.Hr(),
+                html.Br(),
+                html.H3(children='Select upload data', style={'text-align': 'center'}),
+                html.Br(),
+                html.Div(id='uploaded-data', children=[
+                    html.Div(id='up-control-block', children=[
+                        html.Div(children='Select control'),
+                        dcc.Dropdown(
+                            id="control-dropdown",
+                            options=[{'label': i.name, 'value': i.name} for i in test_csvs],
+                            value=0,
+                            style={'width': '140px', 'verticalAlign': "middle",
+                                   'border': 'solid 1px #545454', 'color': 'black'}
+                        ),
+                    ], style={'marginRight': '10px'}),
+                    html.Div(id='up-test-block', children=[
+                        html.Div(children='Select test'),
+                        dcc.Dropdown(
+                            id="test-dropdown",
+                            options=[{'label': i.name, 'value': i.name} for i in test_csvs],
+                            value=0,
+                            style={'width': '140px', 'verticalAlign': "middle",
+                                   'border': 'solid 1px #545454', 'color': 'black'}
+                        ),
+                    ], style={'marginRight': '10px'}),
+                    ], style={'display': 'flex', 'marginTop': '5px',
+                              'justify-content': 'center', 'align-items': 'center'}
+                    ),
+                html.Br(),
+                html.Br(),
+                html.Br(),
+                html.Button(
+                    "Run Selection",
+                    id="run-button",
+                    style={'display': 'block', 'text-align': 'center', 'padding': '10px',
+                           'margin':'auto'}
+                ),
+                ]),
+
+
 def merge_control_test(control_df, test_df, on):
     assert (set(on).issubset(control_df.columns)), 'On columns are not present in control_df'
     assert (set(on).issubset(test_df.columns)), 'On columns are not present in test_df'
@@ -82,6 +169,7 @@ def create_datatable(df_c, df_t):
                         [{"name": ['Test', i.replace("_", " ")], "id": i, "selectable": True} for i in df_m.columns if i.endswith('_test')],
                 data=df_m.to_dict('records'),
                 merge_duplicate_headers=True,
+                filter_action="native",
                 sort_mode="multi",
                 column_selectable="multi",
                 style_table={'height': '525px', 'overflowX': 'scroll', 'overflowY': 'auto'},
@@ -93,14 +181,14 @@ def create_datatable(df_c, df_t):
                     {'if': {'column_id': 'product'},
                         'overflow': 'hidden',
                         'text-overflow': 'ellipsis',
-                        'white-space': 'nowrap'}] + [{'if': {'column_id': str(x)}, 'backgroundColor': '#333652', 'color': 'white'} for x in info_columns] + \
-                                [{'if': {'column_id': str(x)}, 'backgroundColor': '#F6F6F6', 'color': 'black'} for x in df_m.columns if x.endswith('_control')],
+                        'white-space': 'nowrap'}],
                 style_data={
                     'lineHeight': '15px'
                 },
                 page_size=50,
                 sort_action="native",
             )
+
 
 # Main App
 app.layout = html.Div(id='main-app', children=[
@@ -111,91 +199,13 @@ app.layout = html.Div(id='main-app', children=[
                         dcc.Tab(
                             label='About',
                             value='tab1',
-                            children=html.Div(children=[
-                                html.H3(children="PIMMS",
-                                        style={'font-weight': '400', 'font-size': '20pt',
-                                               'margin-bottom': '30px', 'text-align': 'center'}
-                                        ),
-                                dcc.Markdown("""
-                                ### Pragmatic Insertional Mutation Mapping system mapping pipeline
-                                The PIMMS (Pragmatic Insertional Mutation Mapping System) pipeline has been
-                                developed for simple conditionally essential genome discovery experiments in bacteria.
-                                Capable of using raw sequence data files alongside a FASTA sequence of the
-                                reference genome and GFF file, PIMMS will generate a tabulated output of each coding
-                                sequence with corresponding mapped insertions accompanied with normalized results
-                                enabling streamlined analysis. This allows for a quick assay of the genome to identify
-                                conditionally essential genes on a standard desktop computer prioritizing results for
-                                further investigation.
-                                """,
-                                style={'font-weight': '200', 'font-size': '10pt', 'line-height':'1.6'}),
-                                html.Div([
-                                    'Reference: ',
-                                    html.A('PIMMS paper',
-                                           href='https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4391243/pdf/fgene-06-00139.pdf',
-                                           style={'color': '#FFFFFF'})
-                                ]),
-                            ], style={'margin-left': '10px', 'text-align': 'Left'}),
+                            children=control_about_tab(),
                             style={'color': '#000000'}
                         ),
                         dcc.Tab(
                             label='Import Data',
                             value='tab2',
-                            children=html.Div(children=[
-                                        html.Div(children=[
-                                            html.Div(className='bac-dropdown-name', children='Bacterial Species',
-                                                     style={'margin-right': '2em', 'verticalAlign': "middle"}),
-                                            dcc.Dropdown(
-                                                id='bac-species-dropdown',
-                                                options=[
-                                                    {'label': 'Species A', 'value': 'specA'},
-                                                    {'label': 'Species B', 'value': 'specB'},
-                                                    {'label': 'Species C', 'value': 'specC'},
-                                                    {'label': 'Species D', 'value': 'specD'}
-                                                ],
-                                                value='specA',
-                                                style={'width': '140px', 'verticalAlign': "middle",
-                                                       'border': 'solid 1px #545454', 'color': 'black'}
-                                            )
-                                        ], style={'display': 'flex', 'marginTop': '5px', 'justify-content': 'center',
-                                                  'align-items': 'center'}),
-                                        html.Hr(),
-                                        html.Br(),
-                                        html.H3(children='Select upload data', style={'text-align': 'center'}),
-                                        html.Br(),
-                                        html.Div(id='uploaded-data', children=[
-                                            html.Div(id='up-control-block', children=[
-                                                html.Div(children='Select control'),
-                                                dcc.Dropdown(
-                                                    id="control-dropdown",
-                                                    options=[{'label': i.name, 'value': i.name} for i in test_csvs],
-                                                    value=0,
-                                                    style={'width': '140px', 'verticalAlign': "middle",
-                                                           'border': 'solid 1px #545454', 'color': 'black'}
-                                                ),
-                                            ], style={'marginRight': '10px'}),
-                                            html.Div(id='up-test-block', children=[
-                                                html.Div(children='Select test'),
-                                                dcc.Dropdown(
-                                                    id="test-dropdown",
-                                                    options=[{'label': i.name, 'value': i.name} for i in test_csvs],
-                                                    value=0,
-                                                    style={'width': '140px', 'verticalAlign': "middle",
-                                                           'border': 'solid 1px #545454', 'color': 'black'}
-                                                ),
-                                            ], style={'marginRight': '10px'}),
-                                            ], style={'display': 'flex', 'marginTop': '5px',
-                                                      'justify-content': 'center', 'align-items': 'center'}
-                                            ),
-                                        html.Br(),
-                                        html.Br(),
-                                        html.Br(),
-                                        html.Button(
-                                            "Run Selection",
-                                            id="run-button",
-                                            style={'display': 'block', 'text-align': 'center', 'padding': '10px',
-                                                   'margin':'auto'}
-                                        ),
-                                        ]),
+                            children=control_data_tab(),
                             style={'color': '#000000'}
                         ),
                     ]),
@@ -213,7 +223,7 @@ app.layout = html.Div(id='main-app', children=[
                          style={'display': 'inline-block',
                                 'height': '525px',
                                 'margin': '35px',
-                                'width': '1060px',
+                                'width': '65%',
                                 'background-color': '#333652',
                                 'font-size': '10pt',
                                 'color': 'black'}
@@ -250,6 +260,7 @@ def onclick(n_clicks,test_path, control_path):
             df_t = pd.read_csv(test_csvs[control_csv_idx])
             return create_datatable(df_c, df_t)
     return html.Div('Select data for import', style={'color': 'white', 'margin': '10px'})
+
 
 if __name__ == '__main__':
     app.run_server(
