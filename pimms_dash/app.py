c7777f8a974573dc43c1b36baf5f06530bc051ce
Adam Taylor <adam.taylor@nottingham.ac.uk>
2021-04-23 16:51:02 +0100

    update example data path

--- pimms_dash/app.py
+++ pimms_dash/app.py
@@ -17,7 +17,7 @@ logging.basicConfig(level=logging.INFO)
 # Define local paths
 BASE_PATH = pathlib.Path(__file__).parent.resolve()
 DATA_PATH = BASE_PATH.joinpath('data').resolve()
-TESTDATA_PATH = DATA_PATH.joinpath('uploaded').resolve()
+TESTDATA_PATH = DATA_PATH.joinpath('example_data').resolve()
 
 # Plotly standard graph format
 plotly_template = 'simple_white'
