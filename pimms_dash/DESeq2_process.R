4841ed8d2996f7b2e480550c113da09b0c12846d
Adam Taylor <adam.taylor@nottingham.ac.uk>
2021-05-12 15:29:27 +0100

    Process PCs in R script, handle return, save as dict in pimms_df object

--- pimms_dash/DESeq2_process.R
+++ pimms_dash/DESeq2_process.R
@@ -1,9 +1,18 @@
 library(DESeq2)
 
 run_deseq <- function(countsdata, metadata) {
+  # Perform DESeq
   pimms2 <- DESeqDataSetFromMatrix(countData = countsdata, colData = metadata, design =~dex, tidy = TRUE)
   pimms2 <- DESeq(pimms2)
   res <- results(pimms2)
-  df = data.frame(res)
-  return(df)
+  
+  # Get PCA plot dataframe
+  vsdata <- vst(pimms2, blind = FALSE)
+  pca <- plotPCA(vsdata, intgroup="dex")
+  
+  # Create output dataframes
+  deseq_data <- data.frame(res)
+  
+  output_list <- list("deseq"=deseq_data, "pca"=pca)
+  return(output_list)
 }
